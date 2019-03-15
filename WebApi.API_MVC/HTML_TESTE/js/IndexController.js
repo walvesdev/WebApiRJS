e6221ba49f11f99242165cd0be73918f2ae34acd
Willian <Willian@DESKTOP-B1T9NO5>
2019-03-15 12:43:38 -0300

    ok

--- WebApi.API_MVC/HTML_TESTE/js/IndexController.js
+++ WebApi.API_MVC/HTML_TESTE/js/IndexController.js
@@ -36,24 +36,31 @@ class IndexController {
     }
     Salvar() {
         try {
+            var itemId = indexController.GetItemComId();
             var item = indexController.GetItemSemId();
-            if (item.hasOwnProperty(item.id)) {
-                alert('Informe todos os dado para ser cadastrados!')
+
+            if (itemId.id != "") {
+                indexController.Editar(itemId);
             } else {
-                $.ajax({
-                    url: 'http://localhost:5000/api/item',
-                    type: "post",
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    data: JSON.stringify(item)
-                })
-                    .done(function (item) {
-                        indexController.LimparCampos();
-                        location.reload();
+
+                if (item.nome == "" || item.date == "" || item.value == "") {
+                    alert('Informe todos os dado para ser cadastrados!')
+                } else {
+                    $.ajax({
+                        url: 'http://localhost:5000/api/item',
+                        type: "post",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        data: JSON.stringify(item)
                     })
-                    .fail(function () {
-                        alert("Falha ao inserir item!")
-                    })
+                        .done(function (item) {
+                            indexController.LimparCampos();
+                            location.reload();
+                        })
+                        .fail(function () {
+                            alert("Falha ao inserir item!")
+                        })
+                }
             }
         } catch (erro) {
             alert(`Erro ${erro}`)
@@ -76,19 +83,19 @@ class IndexController {
             alert(`Erro ${erro}`)
         }
     }
-    Editar() {
-        if ($('#id').val().trim() != null && $('#id').val().trim() != "") {
+    Editar(itemId) {
+        if (itemId.id != null && itemId.id != "") {
 
-            if ($('#date').val().trim() != null && $('#date').val().trim() != "") {
+            if (itemId.date != null && itemId.date != "") {
                 try {
-                    var item = indexController.GetItemComId();
+                    //var item = indexController.GetItemComId();
 
                     $.ajax({
-                        url: `http://localhost:5000/api/item/${item.id}`,
+                        url: `http://localhost:5000/api/item/${itemId.id}`,
                         type: "put",
                         contentType: "application/json; charset=utf-8",
                         dataType: "json",
-                        data: JSON.stringify(item)
+                        data: JSON.stringify(itemId)
                     })
                         .done(function (item) {
                             indexController.LimparCampos();
@@ -108,13 +115,13 @@ class IndexController {
         }
 
     }
-    Excluir() {
-        if ($('#id').val().trim() != null && $('#id').val().trim() != "" && confirm('Desja excluir o item selecionado?')) {
+    Excluir(id) {
 
-            try {
-                var item = indexController.GetItemComId();
+        try {
+            if (confirm('Desja excluir o item selecionado?')) {
+                //var item = indexController.GetItemComId();
                 $.ajax({
-                    url: `http://localhost:5000/api/item/${item.id}`,
+                    url: `http://localhost:5000/api/item/${id}`,
                     type: "delete"
                 })
                     .done(function (item) {
@@ -124,15 +131,13 @@ class IndexController {
                     .fail(function () {
                         alert("Item não encontrado!")
                     })
-            } catch (erro) {
-                alert(`Erro ${erro}`)
             }
-        } else {
-            alert('Selecione um item para ser excluido!');
+        } catch (erro) {
+            alert(`Erro ${erro}`)
         }
     }
+
     Cancelar() {
-        $("#salvar").prop('disabled', false);
         indexController.LimparCampos();
     }
     LimparCampos() {
@@ -142,6 +147,7 @@ class IndexController {
         $('#value').val("");
         $('#id').val("");
         $('#active').prop('checked', false);
+        $('.linha').removeClass("linha");
 
     }
     PreencherTabela(listaItems) {
@@ -152,7 +158,8 @@ class IndexController {
                 <td>${item.active}</td>
                 <td>${item.name}</td>
                 <td>${item.date}</td>
-                <td>${item.value}</td>   
+                <td>${item.value}</td> 
+                <td><a class="fas fa-pencil-alt editar"></a>   |  <a class="fas fa-trash-alt remover"></a></td>
                 </tr>                         
                 `);
         }
@@ -189,7 +196,13 @@ class IndexController {
                 { "data": "active" },
                 { "data": "name" },
                 { "data": "date", render: function (data) { return (moment(data).format('DD/MM/YYYY')) } },
-                { "data": "value" }
+                { "data": "value" },
+                {
+                    "data": null, render: function () {
+                        return '<i class="fas fa-pencil-alt editar"></i>' + '   |   ' + '<i class="fas fa-trash-alt"></i>';
+
+                    }
+                }
             ]
         });
         indexController.PreencherTotal(dataset);
@@ -213,30 +226,60 @@ class IndexController {
         }
     }
     ItemInputs() {
-        $("#listarTabela tbody").on("click", "tr", function () {
 
-            $('.selected').css('backgroun-color', 'red')
-            $("#salvar").prop('disabled', true);
+        let id;
+        let active;
+        let name;
+        let date;
+        let value;
+        let editar;
+        let excluir;
 
-            var id = $(this).children().first().text();
-            var active = JSON.parse($(this).children().first().next().text());
-            var name = $(this).children().first().next().next().text();
-            var date = $(this).children().first().next().next().next().text();
-            var value = $(this).children().first().next().next().next().next().text();
+        $("#divTabela #listarTabela tbody").on("click", "tr", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
 
-            $('#name').val(name);
-            $('#date').val(date);
-            $('#value').val(value);
-            $('#id').val(id);
-            $('#active').prop('checked', active);
+            $('.linha').removeClass("linha");
+            $(this).addClass("linha");
 
+            id = $(this).children().first().text();
+            active = JSON.parse($(this).children().first().next().text());
+            name = $(this).children().first().next().next().text();
+            date = $(this).children().first().next().next().next().text();
+            value = $(this).children().first().next().next().next().next().text();
+
+            editar = $(this).children().first().next().next().next().next().next().children().first();
+            excluir = $(this).children().first().next().next().next().next().next().children().first().next();
+
+            editar.click(function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                $('#name').val(name);
+                $('#date').val(date);
+                $('#value').val(value);
+                $('#id').val(id);
+                $('#active').prop('checked', active);
+            })
+            excluir.click(function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                indexController.Excluir(id);
+                $('.linha').removeClass("linha");
+                location.reload();
+
+            });
+           
         });
+
+
     }
 }
 
 function main() {
     indexController = new IndexController();
-    indexController.GetDatSet();
+    indexController.BuscarTodos();
     indexController.ItemInputs();
 
 }
